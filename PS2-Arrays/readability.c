7207a27e9c3dc5f148b2846cf5c0d8c944b1bc17
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-08-04 13:10:53 -0500

    readability problem solved

--- PS2-Arrays/readability.c
+++ PS2-Arrays/readability.c
@@ -10,6 +10,8 @@ int main(void)
 {
     //prompt the user for a string of text
     string text = get_string("Text: ");
+    //printf("Text: %s \n",text);
+
     int count_letters, count_words, count_sentences;
     count_letters = 0;
     count_words = 0;
@@ -19,27 +21,43 @@ int main(void)
     {
         //count the number of letters (lowercase from a to z or uppercase from A to Z)
         if (isalpha(text[i]))
+        {
             count_letters++;
+        }
+
         //count the number of words (sequence of characters separated by spaces)
         if ((i == 0 && text[i + 1] != ' ') || (i != len - 1 && text[i] == ' ' && text[i + 1] != ' '))
+        {
             count_words++;
+        }
+
         //count the number of sentences (period, exclamation point or question mark)
         if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
             count_sentences++;
+        }
     }
 
-    float L = count_letters / (count_words / 100)
-
-                                  float index = 0.0588 * L - 0.296 * S - 15.8;
-
-    printf("%d letters(s)\n", count_letters);
-    printf("%d word(s)\n", count_words);
-    printf("%d sentence(s)\n", count_sentences);
+    float L = (count_letters / (float)count_words) * 100;
+    float S = (count_sentences / (float)count_words) * 100;
 
     //Grade rounded to the nearest integer
-    //if grade higher >=16 output -> print "Grade 16+"
-    //if grade <1 output -> print "Before Grade 1"
+    int index = roundf(0.0588 * L - 0.296 * S - 15.8);
 
-    //output
-    //printf("Grade %s \n", grade);
+    //if grade higher >=16 output -> print "Grade 16+"
+
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+        //if grade <1 output -> print "Before Grade 1"
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+        //output
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
