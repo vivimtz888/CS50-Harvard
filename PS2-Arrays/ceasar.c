2bcead9955013cb68c273b34d5000df23f36db54
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-08-04 15:16:01 -0500

    pseudocode for encryption challenge

---
+++ PS2-Arrays/ceasar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+    //prompt the user for a command line -> non-negative integer
+
+    //1. do while k = no command-line or with more than one
+    //printf = error & return from main = 1 (error)
+    if (argc == 2)
+    {
+        printf("Success\n");
+    }
+    else if (argc < 2 || argc > 2)
+    {
+        printf("Usage: ./caesar key\n");
+    }
+
+    //2. for -iterate to check if not a decimal digit -> print Usage::./ceasar key and return from main a value of 1
+    //3. Convert command-line argument from string to int (int)
+
+    //4. output plaintext: (without a new line) printf
+    //prompt the user for a string of plaintext get_string
+    //5. iterate through every character to change it --> for
+    // print out the rotated character (lowcase and uppercase preserve) - non-alphabetical preserve
+    //output ciphertext: (without a new line) with the plaintext in ciphertext
+    //printf a new line and return 0 from main
+}
