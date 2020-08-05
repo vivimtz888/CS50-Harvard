e27114243f31568856fc9581f6b6f028280af56c
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-08-04 20:43:28 -0500

    get key and get plaintext

--- PS2-Arrays/ceasar.c
+++ PS2-Arrays/ceasar.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
     //prompt the user for a command line -> non-negative integer
+    int dec_digit;
 
     //1. do while k = no command-line or with more than one
     //printf = error & return from main = 1 (error)
     if (argc == 2)
     {
-        printf("Success\n");
+        // printf("%c",argv[1][0]);
+
+        //2. for -iterate to check if not a decimal digit -> print Usage::./ceasar key and return from main a value of 1
+        for (int i = 0, len = strlen(argv[1]); i < len; i++)
+        {
+            if (isdigit(argv[1][i]))
+            {
+                //3. Convert command-line argument from string to int (int)
+                dec_digit = atoi(argv[1]);
+            }
+            else
+            {
+                printf("Usage: ./caesar key\n");
+            }
+        }
     }
     else if (argc < 2 || argc > 2)
     {
         printf("Usage: ./caesar key\n");
     }
-
-    //2. for -iterate to check if not a decimal digit -> print Usage::./ceasar key and return from main a value of 1
-    //3. Convert command-line argument from string to int (int)
 
     //4. output plaintext: (without a new line) printf
     //prompt the user for a string of plaintext get_string
