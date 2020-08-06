0a7df82cfa4c633d05486fcb4b4edcfcbeeeb5bb
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-08-06 18:21:45 -0500

    program encrypts messages with a certain key - done

--- PS2-Arrays/ceasar.c
+++ PS2-Arrays/ceasar.c
@@ -26,6 +26,7 @@ int main(int argc, string argv[])
             else
             {
                 printf("Usage: ./caesar key\n");
+                return 1;
             }
         }
     }
@@ -36,8 +37,59 @@ int main(int argc, string argv[])
 
     //4. output plaintext: (without a new line) printf
     //prompt the user for a string of plaintext get_string
+
+    string plaintext = get_string("plaintext: ");
+
+    printf("ciphertext: ");
+
     //5. iterate through every character to change it --> for
-    // print out the rotated character (lowcase and uppercase preserve) - non-alphabetical preserve
-    //output ciphertext: (without a new line) with the plaintext in ciphertext
+
+    int k = atoi(argv[1]);
+
+    //printf("%i", k);
+
+
+    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    {
+        char pl = plaintext[i];
+
+
+        if (isalpha(plaintext[i]))
+        {
+            // check if it is uppercase
+            if (plaintext[i] >= 'A' && plaintext[i] <= 'Z')
+            {
+                char p = plaintext[i] - 65;
+                char c = (p + k) % 26;
+                char encrypted_upper = c + 65;
+                printf("%c", encrypted_upper);
+            }
+            //check if it is downcase
+            else if (plaintext[i] >= 'a' && plaintext[i] <= 'z')
+            {
+                char p = plaintext[i] - 97;
+                char c = (p + k) % 26;
+                char encrypted_lower = c + 97;
+                printf("%c", encrypted_lower);
+            }
+        }
+        // print out the rotated character (lowcase and uppercase preserve) - non-alphabetical preserve
+        else
+        {
+            printf("%c", pl);
+        }
+
+    }
     //printf a new line and return 0 from main
+    printf("\n");
+    return 0;
+
+
+
+
+
+
+    //output ciphertext: (without a new line) with the plaintext in ciphertext
+
+
 }
